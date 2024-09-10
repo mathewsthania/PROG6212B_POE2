860c413ad340790acd2e94666fe7e13c59f66991
mathewsthania <ST10381071@VCCONNECT.EDU.ZA>
2024-09-10 13:18:44 +0200

    Adding in picture for the home page. Adding in submit claim heading for my view. Adding in code for my submit a claim view. I added in a form for the user to submit - hours worked, hourly rate, claim period from, claim period to and uploading supporting documents. Adding in design - CSS.

---
+++ Models/Claim.cs
@@ -0,0 +1,26 @@
+﻿//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*THE*START*OF*FILE*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PROG_POE2.Models
+{
+    public class Claim
+    {
+        public int ClaimID { get; set; }
+
+        public double HoursWorked { get; set; }
+
+        public decimal HourlyRate { get; set; }
+
+        public decimal TotalAmount => (decimal)HoursWorked * HourlyRate;
+
+        public string ClaimMonth { get; set; }
+
+        public string Status { get; set; } = "Pending";
+
+        public string SupportingDocumentPath { get; set; }
+
+    }
+}
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*THE*END*OF*FILE*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
